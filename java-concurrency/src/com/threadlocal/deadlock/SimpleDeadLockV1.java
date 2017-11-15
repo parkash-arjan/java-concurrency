14d1728e040e15a6b91559342281786149872425
parkash-arjan <parkash.arjan.github@gmail.com>
2017-11-15 16:27:51 -0500

    Simple Deadlock example

---
+++ java-concurrency/src/com/threadlocal/deadlock/SimpleDeadLockV1.java
@@ -0,0 +1,22 @@
+package com.threadlocal.deadlock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SimpleDeadLockV1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Lock lock1 = new ReentrantLock();
+		Lock lock2 = new ReentrantLock();
+		Thread t1 = new Thread(new ThreadA(lock1, lock2));
+		Thread t2 = new Thread(new ThreadB(lock1, lock2));
+
+		t1.start();
+		t2.start();
+
+		t1.join();
+		t2.join();
+
+	}
+}
---
+++ java-concurrency/src/com/threadlocal/deadlock/ThreadA.java
@@ -0,0 +1,33 @@
+package com.threadlocal.deadlock;
+
+import java.util.concurrent.locks.Lock;
+
+public class ThreadA implements Runnable {
+	private Lock lock1;
+	private Lock lock2;
+
+	public ThreadA(Lock lock1, Lock lock2) {
+		this.lock1 = lock1;
+		this.lock2 = lock2;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Start -> run()::ThreadA");
+
+		lock1.lock();
+		System.out.println("lock1.lock() -> run()::ThreadA");
+
+		lock2.lock();
+		System.out.println("lock2.lock() -> run()::ThreadA");
+
+		lock2.unlock();
+		System.out.println("lock2.unlock() -> run()::ThreadA");
+
+		lock1.unlock();
+		System.out.println("lock1.unlock() -> run()::ThreadA");
+
+		System.out.println("End -> run()::ThreadA");
+	}
+
+}
